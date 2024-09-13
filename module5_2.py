ac91cc3c1d545e83c5d1407bc2acafdf5cef1e5e
oceanbig093 <innervision41@mail.ru>
2024-09-13 16:52:51 +0300

    Add files via upload

---
+++ module5_2.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+h1 = House('ЖК Малиновка', 15)
+h2 = House('ЖК Смородиновка', 7)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
